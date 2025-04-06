e7784a4d3b0ba87541989f989761c13eff2d12c5
K <mango1472@outlook.kr>
2025-04-06 17:34:38 +0900

    Update wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+
+app = application  # Vercel에서 찾을 수 있도록 app 변수 추가
